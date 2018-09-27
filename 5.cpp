53abc463215b40a180ded51af202eda41be02448
Diljot Singh <diljots99@gmail.com>
2018-09-27 13:06:21 +0530

    .

---
+++ 5.cpp
@@ -0,0 +1,35 @@
+/*
+*To find the no. is even or odd.
+*/
+#include "iostream"
+
+using namespace std;
+class evenOdd
+{
+    int number;
+    public:
+    evenOdd()
+    {
+        cout<<"\t\t\tEVEN ODD"<<"\nEnter the value:- ";
+        cin>>number;
+        if((number%2)==0)
+        {
+            cout<<number<<" is Even\n";
+        }
+        else
+        {
+            {
+            cout<<number<<" is odd\n";
+        }
+
+        }
+
+    }
+};
+
+int main()
+{
+    new evenOdd;
+    system("pause");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# OOPS-Sem
+# OOPS-Sem3
+This Project Contains all the files for the award degree of B.Tech in CSE
+for the subject  
