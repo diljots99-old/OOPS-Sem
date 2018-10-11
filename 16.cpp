4eaf0cc9e8e80a7bfdc7f44d7f3404d2bf5f2c8f
Diljot Singh <diljots99@gmail.com>
2018-10-11 19:51:24 +0530

    Convert Decimal To Binary

--- 16.cpp
+++ 16.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()  
 {  
-    int a[128],b[128],k, n, i,j;    
+    int a[128], n, i,j;    
     cout<<"\nEnter the number to convert: ";    
     cin>>n;    
     for(i=0; n>0; i++)    
@@ -15,19 +15,16 @@ int main()
         a[i]=n%2;    
         n= n/2;  
     }
-    k=0+i;    
+    int b[i]; 
     cout<<"\nBinary of the given number= ";    
     for(i=i-1,j=0 ;i>=0 ;i--,j++)         
     {    
         b[j]=a[i];
-          
     }
-    i=0;
-    while(k)
+    for(i=0;i<(sizeof(b)/sizeof(b[0]));i++)
     {
         cout<<b[i];
-        i++;
-        k--;
+        
     }
     cout<<"\n";    
     system("pause");
