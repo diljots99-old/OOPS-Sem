bb366c5f946969816cf816cda39a0b06f119f119
Diljot Singh <diljots99@gmail.com>
2018-10-11 23:11:11 +0530

    Sum of three numbers using functions

---
+++ 19.cpp
@@ -0,0 +1,22 @@
+/*
+*Sum of three numbers using functions
+*/
+#include "iostream"
+
+using namespace std;
+
+int sum(int a,int b,int c)
+{
+    int rst;
+    rst=a+b+c;
+    return rst;
+}
+
+int main()
+{
+    int num1,num2,num3;
+    cout<<"SUM OF THREE NUMBERS USING FUNCTION\n Enter the number :-";
+    cin>>num1>>num2>>num3;
+    cout<<"Result :- " <<sum(num1,num2,num3);
+    return 0;
+}
